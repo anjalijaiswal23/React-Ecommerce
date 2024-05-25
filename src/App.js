60e0f1ca151d8f83854e8ca96308dccd432c0163
w3villa-anjali-jaiswal <anjali.jaiswal@w3villa.com>
2024-05-25 17:31:26 +0530

    add_ui_for_login_sign_UP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Login from './Authorised/Login';
+import Registration from './Authorised/Registration';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-          Learn React
+         <Registration/>
+         <Login/>
       </header>
     </div>
   );
